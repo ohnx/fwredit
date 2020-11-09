d6fbdad62d97808d52dc417003155267667e736c
Mason <me@masonx.ca>
2020-11-09 07:53:54 +0000

    Begin work on WebSockets (for multi-robot environment)

--- backend/app.js
+++ backend/app.js
@@ -2,12 +2,21 @@ const http = require('http');
 const url = require('url');
 const fs = require('fs');
 const spawn = require('child_process').spawn;
+const WebSocketServer = require('websocket').server;
 const path = require('path');
 const tmp = require('tmp');
 const initWd = process.cwd();
 
 const HOST = process.env.HOST || '127.0.0.1';
 const PORT = process.env.PORT || 8080;
+
+let niceDateTime = function() {
+  let padToFour = number => number <= 99 ? `0${number}`.slice(-2) : number;
+  let d = new Date();
+  let s = `[${d.getFullYear()}-${padToFour(d.getMonth()+1)}-${padToFour(d.getDate())}]`;
+  s += `[${padToFour(d.getHours())}:${padToFour(d.getMinutes())}:${padToFour(d.getSeconds())}]`;
+  return s;
+}
 
 console.log(`Server listening on ${HOST}:${PORT}`);
 
@@ -34,7 +43,7 @@ let compileCode = function(codePath, res, cb) {
   let fsOutputPath = path.join(initWd, 'compile_out', outputPath);
   let childStdout = '', childStderr = '';
 
-  console.log(`Compiling ${codePath} to ${fsOutputPath}`);
+  console.log(`${niceDateTime()} Compiling ${codePath} to ${fsOutputPath}`);
 
   let child = spawn('emcc', [codePath,
                     '-I', 'util/',
@@ -76,7 +85,7 @@ let compileCode = function(codePath, res, cb) {
   });
 };
 
-http.createServer(function (req, res) {
+let httpServer = http.createServer(function (req, res) {
   if (req.method === 'POST') {
     if (!checkAuth(req)) {
       res.writeHead(400, {'Content-Type': 'text/json'});
@@ -114,4 +123,86 @@ http.createServer(function (req, res) {
     res.writeHead(400, {'Content-Type': 'text/json'});
     res.end(`{"msg":"Invalid request"}`);
   }
-}).listen(PORT, HOST);
+});
+
+let wsServer = new WebSocketServer({
+  httpServer: httpServer
+});
+
+let rooms = {};
+const MSG_SUB = 1;
+const MSG_UNSUB = 2;
+const MSG_SEND = 3;
+
+let subscribe_room = function(conn, room) {
+  if (!(room in rooms)) {
+    rooms[room] = [];
+  }
+
+  // add connection to the room
+  rooms[room].push(conn);
+};
+
+let unsubscribe_room = function(conn, room) {
+  if (!(room in rooms)) { return; }
+
+  // linear search room
+  for (var i = 0; i < rooms[room].length; i++) {
+    if (rooms[room][i] == conn) {
+      rooms[room].splice(i, 1);
+      return;
+    }
+  }
+};
+
+let broadcast_room = function(conn, room, msg) {
+  if (!(room in rooms)) { return; }
+
+  // linear search room
+  for (var i = 0; i < rooms[room].length; i++) {
+    if (rooms[room][i] == conn) {
+      // don't echo back
+      continue;
+    } else {
+      if (rooms[room][i].connected) {
+        connection.sendUTF(msg);
+      }
+    }
+  }
+};
+
+wsServer.on('request', function(request) {
+  var connection = request.accept('funwithrobots', request.origin);
+
+  console.log(`${niceDateTime()} Connection accepted from ${connection.remoteAddress}`);
+
+  // This is the most important callback for us, we'll handle
+  // all messages from users here.
+  connection.on('message', function(message) {
+    if (message.type === 'utf8') {
+      let d;
+      try { d = JSON.parse(message.utf8Data); d.type; d.room; } catch(e) { return; }
+      switch (d.type) {
+        case MSG_SUB:
+          subscribe_room(connection, d.room);
+          break;
+        case MSG_UNSUB:
+          unsubscribe_room(connection, d.room);
+          break;
+        case MSG_SEND:
+          broadcast_room(connection, d.room, message.utf8Data);
+          break;
+      }
+    }
+  });
+
+  connection.on('close', function(reasonCode, description) {
+    console.log(`${niceDateTime()} ${connection.remoteAddress} disconnected`);
+    // unsubscribe from all rooms (super inefficient, ik)
+    for (var room in rooms) {
+      if (rooms.hasOwnProperty(room)) unsubscribe_room(connection, room);
+    }
+  });
+});
+
+httpServer.listen(PORT, HOST);
