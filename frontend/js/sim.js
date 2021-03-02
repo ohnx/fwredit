d4e29727599b13b9158aeb96ede9d9fce3d7cb27
ohnx <me@masonx.ca>
2021-03-02 00:59:47 +0000

    add photovore lab (untested)

--- frontend/js/sim.js
+++ frontend/js/sim.js
@@ -7,6 +7,20 @@ const WORLD_MAX = 200;
 // number of pins
 const NUM_PINS = 20;
 
+let randomizeLightPosn = function() {
+  let newLightPosn = {xPos: 0, zPos: 0};
+
+  var xSgn = Math.random() > 0.5;
+  var xPos = Math.random() * (WORLD_MAX/2 - 50) + 40;
+  newLightPosn.xPos = xSgn ? xPos : -xPos;
+  var zSgn = Math.random() > 0.5;
+  var zPos = Math.random() * (WORLD_MAX/2 - 50) + 40;
+  newLightPosn.zPos = zSgn ? zPos : -zPos;
+
+  localStorage.setItem('LIGHT_POSITION', JSON.stringify(newLightPosn));
+  return newLightPosn;
+};
+
 let simulatorCode = function(Ammo) {
   // - Global variables -
   var DISABLE_DEACTIVATION = 4;
@@ -74,6 +88,9 @@ let simulatorCode = function(Ammo) {
 
   if (current_lab == 'lab1') {
     lab_init = lab1_square_setup;
+  } else if (current_lab == 'lab2') {
+    lab_init = lab2_photovore_setup;
+    lab_syncfunc = lab2_photovore_syncfunc;
   } else if (current_lab == 'lab3') {
     lab_init = lab3_control_setup;
     lab_syncfunc = lab3_control_syncfunc;
@@ -88,6 +105,162 @@ let simulatorCode = function(Ammo) {
     for (var j = 0; j < nw; j++)
       for (var i = 0; i < nh; i++)
         createBox(new THREE.Vector3(size * j - (size * (nw - 1)) / 2, size * i, 10), ZERO_QUATERNION, size, size, size, 10);
+  }
+
+  // LAB 2: PHOTOVORE LAB
+  var lab2_light;
+  var leftSensorOffset;
+  var rightSensorOffset;
+  const NUM_RAYS = 50;
+  const LEFT_START = 45;
+  const RIGHT_START = 5;
+  const LEFT_END = LEFT_START - NUM_RAYS;
+  const RIGHT_END = RIGHT_START - NUM_RAYS;
+  let zBase = new THREE.Vector3(0, 1, 0);
+  var debugArrows = new Array();
+
+  function lab2_photovore_setup() {
+    // the light
+    var geometry = new THREE.SphereBufferGeometry(3, 32, 32);
+    var material = new THREE.MeshBasicMaterial({color: 0xffff00});
+    lab2_light = new THREE.Mesh(geometry, material);
+    // store the last used position of the light
+    let lastLightPosn = localStorage.getItem('LIGHT_POSITION');
+    if (lastLightPosn) {
+      lastLightPosn = JSON.parse(lastLightPosn);
+    } else {
+      lastLightPosn = randomizeLightPosn();
+    }
+    lab2_light.position.set(lastLightPosn.xPos, 2, lastLightPosn.zPos);
+    scene.add(lab2_light);
+
+    // the sensors' offsets (as a percentage of the main reading from -15% to 15%)
+    leftSensorOffset = localStorage.getItem('LEFT_SENSOR_OFFSET');
+    if (!leftSensorOffset) {
+      leftSensorOffset = 0.85 + Math.random() * 0.30; // random offset
+      localStorage.setItem('LEFT_SENSOR_OFFSET', leftSensorOffset);
+    }
+    rightSensorOffset = localStorage.getItem('RIGHT_SENSOR_OFFSET');
+    if (!rightSensorOffset) {
+      rightSensorOffset = 0.85 + Math.random() * 0.30; // random offset
+      localStorage.setItem('RIGHT_SENSOR_OFFSET', rightSensorOffset);
+    }
+  }
+
+  function lab2_photovore_syncfunc(p, q) {
+    // raycast to see if we hit a box
+    carPosition.set(p.x(), p.y(), p.z());
+    forwardsDirection.set(q.x(), q.y(), q.z(), q.w());
+    var vehiclePoint = new THREE.Vector3(0, 0, 1);
+    vehiclePoint.applyQuaternion(forwardsDirection);
+
+    // DEBUG: show arrows
+    if (arrow) scene.remove(arrow);
+    arrow = new THREE.ArrowHelper(vehiclePoint, carPosition, 100, 0xff0000);
+    scene.add(arrow);
+    if (debugArrows.length > 0) {
+      for (var dAi = 0; dAi < debugArrows.length; dAi++) {
+        scene.remove(debugArrows[dAi]);
+      }
+      // reset the array
+      debugArrows = [];
+    }
+
+    // fancy stuff to raytrace to the light now!
+    var leftSensorValue = 0;
+    var rightSensorValue = 0;
+
+    var leftLightStartingPosn = carPosition.clone();
+    var llSPtiltedQuat = new THREE.Quaternion();
+    llSPtiltedQuat.setFromAxisAngle(zBase, 45 * Math.PI / 180);
+    var llSPtiltedVec = vehiclePoint.clone();
+    llSPtiltedVec.applyQuaternion(llSPtiltedQuat);
+    leftLightStartingPosn.add(llSPtiltedVec);
+
+    var rightLightStartingPosn = carPosition.clone();
+    var rlSPtiltedQuat = new THREE.Quaternion();
+    rlSPtiltedQuat.setFromAxisAngle(zBase, -45 * Math.PI / 180);
+    var rlSPtiltedVec = vehiclePoint.clone();
+    rlSPtiltedVec.applyQuaternion(rlSPtiltedQuat);
+    rightLightStartingPosn.add(rlSPtiltedVec);
+
+    var traceRay = function(isLeft, angle) {
+      var tiltedQuat = new THREE.Quaternion();
+      tiltedQuat.setFromAxisAngle(zBase, angle * Math.PI / 180);
+      var tiltedVec = vehiclePoint.clone();
+      tiltedVec.applyQuaternion(tiltedQuat);
+      var startingPos = isLeft ? leftLightStartingPosn : rightLightStartingPosn;
+
+      // debug - show the rays
+      if (isLeft)
+        debugArrows.push(new THREE.ArrowHelper(tiltedVec, startingPos, 100, 0x3300ff));
+      else
+        debugArrows.push(new THREE.ArrowHelper(tiltedVec, startingPos, 100, 0x00ffff));
+      scene.add(debugArrows[debugArrows.length - 1]);
+
+      // trace the ray
+      raycaster.set(startingPos, tiltedVec);
+      var intersects = raycaster.intersectObjects([lab2_light]);
+      if (intersects.length > 0) {
+        var intersect = intersects[0];
+        // ookayy time to compute the value of the brightness!
+        var baseBrightness = 0;
+        // first off is the distance. max distance is 200, but if we
+        // collide, i still want to show at least a little bit vs. 0.
+        // at some point, we want to have near-1024
+        // for now, let's try the equation 1000000/(x+60)^2
+        // (we want to decay the brightness squared; this seems most realistic)
+        var iDr = intersect.distance + 60;
+        baseBrightness = 5000000/(iDr * iDr);
+        // next, we want to consider the angle at which the ray is cast.
+        // the more "off" it is, the less it should count.
+        // +/- 25 is the midpoint here
+        var offNess = 0;
+        if (isLeft) offNess = angle + 25;
+        else offNess = angle - 25;
+        offNess = Math.abs(offNess);
+        // so the range of offness is [0, 25]... at 0 offness, we want to keep
+        // the value at 1.
+        // for now, let's try the equation 1-0.0002x^2
+        baseBrightness *= (1 - (0.0002 * offNess * offNess));
+        // finally, let's add a bit of noise. readings can vary a little bit sometimes
+        // maybe only 2% noise
+        baseBrightness *= 0.98 + Math.random() * 0.04;
+        // ok! all ready to add this to the value
+        if (isLeft) leftSensorValue += baseBrightness;
+        else rightSensorValue += baseBrightness;
+      }
+    };
+
+    var i = 0;
+    // left side
+    for (i = LEFT_START; i >= LEFT_END; i--) {
+      traceRay(true, i);
+    }
+
+    // right side
+    for (i = RIGHT_START; i >= RIGHT_END; i--) {
+      traceRay(false, i);
+    }
+
+    // factor in the offsets
+    leftSensorValue *= leftSensorOffset;
+    rightSensorValue *= rightSensorOffset;
+
+    // now the brightnesses should be adjusted. float-ize them and cap them
+    leftSensorValue = Math.round(leftSensorValue);
+    if (leftSensorValue < 0) leftSensorValue = 0;
+    if (leftSensorValue > 1023) leftSensorValue = 1023;
+
+    rightSensorValue = Math.round(rightSensorValue);
+    if (rightSensorValue < 0) rightSensorValue = 0;
+    if (rightSensorValue > 1023) rightSensorValue = 1023;
+
+    // ok, now set the results to the analog pins!
+    // 16 = A2 = left sensor
+    pinValues[16] = leftSensorValue;
+    // 14 = A0 = right sensor
+    pinValues[14] = rightSensorValue;
   }
 
   // LAB 3: CONTROL LAB CODE
@@ -151,7 +324,7 @@ let simulatorCode = function(Ammo) {
 
   var debugArrows = new Array();
   var lastRobotState = false;
-  let zBase = new THREE.Vector3(0, 1, 0);
+  zBase = new THREE.Vector3(0, 1, 0);
   const EMITTERS_BASE = 5;
   function lab3_control_syncfunc(p, q) {
     // raytrace to the track
